cb83c0ad8bf974b27b97601292a37c578ba2998e
atomic-zero <lkpanio25@gmail.com>
2025-01-05 03:42:13 +0800

    hajajajajaja

---
+++ public/nav.js
@@ -0,0 +1,9 @@
+        function toggleNav() {
+            document.getElementById('navMenu').classList.toggle('hidden');
+            document.getElementById('overlay').classList.toggle('hidden');
+        }
+
+        function closeNav() {
+            document.getElementById('navMenu').classList.add('hidden');
+            document.getElementById('overlay').classList.add('hidden');
+        }
