866b71f1a3f74ca941b7573ff36f73a189f1511c
atomic-zero <lkpanio25@gmail.com>
2025-01-04 22:07:48 +0800

    ahhaahhaah

--- script/ai-reka.js
+++ script/ai-reka.js
@@ -2,12 +2,13 @@ module.exports["config"] = {
     name: "reka",
     info: "chat with reka ai",
     usage: "[prompt]",
+    isPrefix: false,
     credits: "Kenneth Panio",
     cd: 6,
     type: "chatbot",
 }
 
-module.exports["run"] = async ({ chat, event, font, args }) => {
+module.exports["run"] = async ({ chat, font, args }) => {
     const reply = txt => {
         chat.reply(font.thin(txt));
     }
@@ -21,7 +22,7 @@ module.exports["run"] = async ({ chat, event, font, args }) => {
         const url = 'https://chat.reka.ai/api/chat';
         const headers = {
             'Content-Type': 'application/json',
-            'Authorization': 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjlkbnNsSDdfZlJNNWRXNkFlc1piSiJ9.eyJpc3MiOiJodHRwczovL2F1dGgucmVrYS5haS8iLCJzdWIiOiJhdXRoMHw2Nzc5MjJhZGIwNTIzY2E4MTI0NGI3YjYiLCJhdWQiOlsiaHR0cHM6Ly9hcGkucmVrYS5haSIsImh0dHBzOi8vcHJvZHVjdGlvbi1yZWthLWFpLmV1LmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE3MzU5OTIwODYsImV4cCI6MTczNjA3ODQ4Niwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCBvZmZsaW5lX2FjY2VzcyIsImF6cCI6ImJhcTRRMThvU3NCaVdOd0RhTkM0bTVoZmdRR3dVdXBNIn0.KhqxqmLTRGC-4hvhIZgQK9BryGuxn0IM5k6QXjZgfCg7q2whv5QOtubiUKKqMPeii9HWeAiXi0NH0h33ap5bTLU4DQ4nqm61HsfzSvUu2mWWHd5kMarHNgzlSaDtOkYPkjtAMEsGgn4OWjYempAkTNdOVFcf3IbtIsbqxz0PjVwzLGVJ5k8ndqkOX6G86RQXw3qVoe1i2S9SbLjggrH4_sLCyrrlRlCbKg9TSoG4bti6And-pv2p-tZ1syaR7jkgictBlp_OA1H9_DBV27v9_bzZ_bdnYGwFCH95Ljp1rroxFhRJgd-VEiJFUp8rs_XEW7dwxaCIcv-MM4ldOfAdTA',
+            'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjlkbnNsSDdfZlJNNWRXNkFlc1piSiJ9.eyJpc3MiOiJodHRwczovL2F1dGgucmVrYS5haS8iLCJzdWIiOiJhdXRoMHw2Nzc5MjJhZGIwNTIzY2E4MTI0NGI3YjYiLCJhdWQiOlsiaHR0cHM6Ly9hcGkucmVrYS5haSIsImh0dHBzOi8vcHJvZHVjdGlvbi1yZWthLWFpLmV1LmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE3MzU5OTIwODYsImV4cCI6MTczNjA3ODQ4Niwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCBvZmZsaW5lX2FjY2VzcyIsImF6cCI6ImJhcTRRMThvU3NCaVdOd0RhTkM0bTVoZmdRR3dVdXBNIn0.KhqxqmLTRGC-4hvhIZgQK9BryGuxn0IM5k6QXjZgfCg7q2whv5QOtubiUKKqMPeii9HWeAiXi0NH0h33ap5bTLU4DQ4nqm61HsfzSvUu2mWWHd5kMarHNgzlSaDtOkYPkjtAMEsGgn4OWjYempAkTNdOVFcf3IbtIsbqxz0PjVwzLGVJ5k8ndqkOX6G86RQXw3qVoe1i2S9SbLjggrH4_sLCyrrlRlCbKg9TSoG4bti6And-pv2p-tZ1syaR7jkgictBlp_OA1H9_DBV27v9_bzZ_bdnYGwFCH95Ljp1rroxFhRJgd-VEiJFUp8rs_XEW7dwxaCIcv-MM4ldOfAdTA',
             'User-Agent': 'Mozilla/5.0 (Linux; Android 12; Infinix X669 Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/132.0.6834.56 Mobile Safari/537.36',
             'Referer': 'https://chat.reka.ai/chat/DHhxJnM36J3VDQgwOAZS'
         };
