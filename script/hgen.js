240fa5c74bfaf4b144877becc988e2026c9c898a
atomic-zero <lkpanio25@gmail.com>
2025-01-10 07:07:02 +0800

    bro

--- script/hgen.js
+++ script/hgen.js
@@ -41,7 +41,7 @@ module.exports["run"] = async ({ chat, args, font }) => {
         generating.unsend();
 
          chat.reply({
-                attachment: await chat.stream(imageUrls)
+                attachment: await chat.stream(data.combinedImage)
             });
 
     } catch (error) {
