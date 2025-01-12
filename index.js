bde11328ac1127192395f291a7d249a121fed0d9
atomic-zero <lkpanio25@gmail.com>
2025-01-12 15:50:20 +0800

    bro

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const { spawn, fork } = require("child_process");
+const { spawn } = require("child_process");
 const path = require('path');
-const cluster = require('cluster');
-const os = require('os');
 
 const SCRIPT_FILE = "kokoro.js";
 const SCRIPT_PATH = path.join(__dirname, SCRIPT_FILE);
 
-const MAX_MEMORY_USAGE = Number.MAX_SAFE_INTEGER;
+const MAX_MEMORY_USAGE = Number.MAX_SAFE_INTEGER || 2000 * 1024 * 1024; // 2000 MB
 
-function startWorker() {
-    const mainProcess = spawn("node", [SCRIPT_PATH], {
+let mainProcess;
+
+function start() {
+    mainProcess = spawn("node", [SCRIPT_PATH], {
         cwd: __dirname,
         stdio: "inherit",
         shell: true
@@ -22,16 +22,16 @@ function startWorker() {
     mainProcess.on("close", (exitCode) => {
         if (exitCode === 0) {
             console.log(`STATUS: [${exitCode}] - Process Exited > SYSTEM Rebooting!...`);
-            startWorker();
+            start();
         } else if (exitCode === 1) {
             console.log(`ERROR: [${exitCode}] - System Rebooting!...`);
-            startWorker();
+            start();
         } else if (exitCode === 137) {
             console.log(`POTENTIAL DDOS: [${exitCode}] - Out Of Memory Restarting...`);
-            startWorker();
+            start();
         } else if (exitCode === 134) {
             console.log(`REACHED HEAP LIMIT ALLOCATION: [${exitCode}] - Out Of Memory Restarting...`);
-            startWorker();
+            start();
         } else {
             console.error(`[${exitCode}] - Process Exited!`);
         }
@@ -40,6 +40,7 @@ function startWorker() {
     // Monitor memory usage
     const memoryCheckInterval = setInterval(() => {
         const memoryUsage = process.memoryUsage().heapUsed;
+        /*  console.log(`Current memory usage: ${(memoryUsage / 1024 / 1024).toFixed(2)} MB`);*/
 
         if (memoryUsage > MAX_MEMORY_USAGE) {
             console.error(`Memory usage exceeded ${MAX_MEMORY_USAGE / 1024 / 1024} MB. Restarting server...`);
@@ -55,35 +56,19 @@ function startWorker() {
 
 function gracefulShutdown() {
     console.log("Starting graceful shutdown...");
-    for (const worker of Object.values(cluster.workers)) {
-        worker.kill('SIGTERM');
+    if (mainProcess && mainProcess.pid) {
+        mainProcess.kill('SIGTERM');
     }
 }
 
-if (cluster.isMaster) {
-    // Fork workers for each CPU core
-    const numCPUs = os.cpus().length;
-    console.warn(`Master process: Forking ${numCPUs} workers...`);
+process.on('SIGINT', () => {
+    gracefulShutdown();
+    process.exit(0);
+});
 
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
+process.on('SIGTERM', () => {
+    gracefulShutdown();
+    process.exit(0);
+});
 
-    // Monitor workers
-    cluster.on('exit', (worker, code, signal) => {
-        console.warn(`Worker ${worker.process.pid} died with code ${code}, restarting...`);
-        cluster.fork(); // Restart the worker
-    });
-
-    process.on('SIGINT', () => {
-        gracefulShutdown();
-        process.exit(0);
-    });
-
-    process.on('SIGTERM', () => {
-        gracefulShutdown();
-        process.exit(0);
-    });
-} else {
-    startWorker();
-}
+start();
