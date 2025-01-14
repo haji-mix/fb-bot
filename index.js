7c2f651f91a249c9e62c73331db83c96806802b6
atomic-zero <lkpanio25@gmail.com>
2025-01-14 20:21:18 +0800

    brk

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const {
-    spawn
-} = require("child_process");
+const { spawn } = require("child_process");
 const path = require('path');
 
 const SCRIPT_FILE = "kokoro.js";
@@ -11,17 +9,25 @@ let mainProcess;
 // Always restart if process.env.PID is not '0' (default to true if not provided)
 const restartEnabled = process.env.PID !== '0';
 
+// Set the DEBUG environment variable for logging purposes
+const debugNamespace = "hajime:*"; // You can set this to a specific namespace or use `hajime:*` to log all
+const debugEnv = {
+    ...process.env,  // Include all existing environment variables
+    DEBUG: debugNamespace // Add the DEBUG environment variable
+};
+
 function start() {
     console.log("Starting main process...");
 
-    // Start the main process without memory allocation flags
+    // Start the main process with the debug environment variable
     mainProcess = spawn("node", [
-        "--no-warnings",
+        "--no-warnings", 
         SCRIPT_PATH
     ], {
         cwd: __dirname,
         stdio: "inherit",
-        shell: true
+        shell: true,
+        env: debugEnv // Pass the debug environment variable to the child process
     });
 
     // Handle errors in the main process
