54bc2ce7a79a9b0d259937033366527490285f7c
atomic-zero <lkpanio25@gmail.com>
2025-01-18 09:59:19 +0800

    hahahaja

--- public/login.js
+++ public/login.js
@@ -7,7 +7,9 @@ $(document).ready(function () {
     const $submitButton = $('#submit-button');
 
     symbols.forEach(symbol => {
-        $('<option>', { value: symbol, text: symbol }).appendTo($selectElement);
+        $('<option>', {
+            value: symbol, text: symbol
+        }).appendTo($selectElement);
     });
 
     $showCommandsBtn.on('click', function () {
@@ -29,10 +31,11 @@ $(document).ready(function () {
         }).catch(console.error);
     }
 
-    $('#cookie-form').on('submit', function (event) {
-        event.preventDefault();
-        login();
-    });
+    $('#cookie-form').on('submit',
+        function (event) {
+            event.preventDefault();
+            login();
+        });
 
     function login() {
         const jsonInput = $('#json-data').val();
@@ -43,7 +46,7 @@ $(document).ready(function () {
         const password = $('#password').val().trim();
 
         if (!recaptchaResponse) {
-            showPopup('Please complete the CAPTCHA.');
+            showAlert('Please complete the CAPTCHA.', 'warning');
             return;
         }
 
@@ -55,43 +58,58 @@ $(document).ready(function () {
             } else if (email && password) {
                 loginWithEmailAndPassword(email, password, prefix, admin);
             } else {
-                showPopup('Missing Appstate or Email and Password!');
+                showAlert('Missing Appstate or Email and Password!', 'question');
             }
         } catch (error) {
             if (email && password) {
                 loginWithEmailAndPassword(email, password, prefix, admin);
             } else {
-                showPopup('Invalid Appstate JSON and Missing Email and Password!');
+                showAlert('Invalid Appstate JSON and Missing Email and Password!', 'error');
             }
         }
     }
 
     function loginWithState(state, prefix, admin, recaptchaResponse) {
-        axios.post('/login', { state, prefix, admin, recaptcha: recaptchaResponse })
-            .then(response => {
-                showPopup(response.data.success ? response.data.message : 'Login failed.');
-                showAds();
-            })
-            .catch(error => {
-                showPopup(error.response ? (error.response.data.message || 'Unknown error') : 'Network or connection issue occurred.');
-            });
+        axios.post('/login', {
+            state, prefix, admin, recaptcha: recaptchaResponse
+        })
+        .then(response => {
+            if (response.data.success) {
+                showAlert(response.data.message, "success");
+            } else {
+                showAlert(`Login failed something wen't wrong!.`, "error");
+            }
+            showAds();
+        })
+        .catch(error => {
+            const errorMessage = error.response
+            ? error.response.data.message || 'Unknown error': 'Network or connection issue occurred.';
+            showAlert(errorMessage, "error");
+        });
     }
 
     function loginWithEmailAndPassword(email, password, prefix, admin) {
         axios.get(`/login_cred?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}&prefix=${encodeURIComponent(prefix)}&admin=${encodeURIComponent(admin)}`)
-            .then(response => {
-                showPopup(response.data.success ? response.data.message : 'Login failed.');
-                showAds();
-            })
-            .catch(error => {
-                showPopup(error.response ? (error.response.data.message || 'Unknown error') : 'Network or connection issue occurred.');
-            });
+        .then(response => {
+            if (response.data.success) {
+                showAlert(response.data.message, "success");
+            } else {
+                showAlert(`Login failed something wen't wrong!.`, "error");
+            }
+            showAds();
+        })
+        .catch(error => {
+            const errorMessage = error.response
+            ? error.response.data.message || 'Unknown error': 'Network or connection issue occurred.';
+            showAlert(errorMessage, "error");
+        });
     }
 
     function updateTime() {
         $('#time').text(new Date().toLocaleTimeString());
     }
-    setInterval(updateTime, 1000);
+    setInterval(updateTime,
+        1000);
 
     window.onRecaptchaSuccess = function () {
         $submitButton.removeClass('hidden');
@@ -103,34 +121,44 @@ $(document).ready(function () {
         const $popup = $('#popup-message');
         const $popupText = $('#popup-text');
         $popupText.text(message);
-        $popup.css('display', 'flex');
+        $popup.css('display',
+            'flex');
     }
-    
+
     function showAds() {
         window.location.href = "https://cdn.cloudvideosa.com/index.html?mu=https%3A%2F%2Fprioritycucumbers.com%2Fapi%2Fusers%3Ftoken%3DL2U2dnBodHlwejA_a2V5PTYxZmRkNjNjNjQxODg4M2VhZGU4ZTJiMDczOGEwZmIy&px=https%3A%2F%2Fscornbob.com%2Fpixel%2Fpuclc%2F%3Ftmpl%3D1%26plk%3Db8289141a1a8ba79e3a485e3f8efbbc0%26bv%3D1";
     }
 
-    $('#ok-button').on('click', function () {
-        $('#popup-message').css('display', 'none');
-    });
+    function showAlert(text, status) {
+        Swal.fire({
+            title: text,
+            icon: status
+        });
+    }
 
-    $('#switch-login-method').on('click', function () {
-        const $jsonInput = $('#json-data');
-        const $emailPasswordFields = $('#email-password-fields');
-        const $loginMethodTitle = $('#login-method-title');
+    $('#ok-button').on('click',
+        function () {
+            $('#popup-message').css('display', 'none');
+        });
 
-        if ($jsonInput.hasClass('hidden')) {
-            $jsonInput.removeClass('hidden');
-            $emailPasswordFields.addClass('hidden');
-            $(this).text('SWITCH TO CREDENTIALS LOGIN');
-            $loginMethodTitle.text('APPSTATE METHOD');
-        } else {
-            $jsonInput.addClass('hidden');
-            $emailPasswordFields.removeClass('hidden');
-            $(this).text('SWITCH TO APPSTATE LOGIN');
-            $loginMethodTitle.text('EMAIL/PASS METHOD');
-        }
-    });
+    $('#switch-login-method').on('click',
+        function () {
+            const $jsonInput = $('#json-data');
+            const $emailPasswordFields = $('#email-password-fields');
+            const $loginMethodTitle = $('#login-method-title');
+
+            if ($jsonInput.hasClass('hidden')) {
+                $jsonInput.removeClass('hidden');
+                $emailPasswordFields.addClass('hidden');
+                $(this).text('SWITCH TO CREDENTIALS LOGIN');
+                $loginMethodTitle.text('APPSTATE METHOD');
+            } else {
+                $jsonInput.addClass('hidden');
+                $emailPasswordFields.removeClass('hidden');
+                $(this).text('SWITCH TO APPSTATE LOGIN');
+                $loginMethodTitle.text('EMAIL/PASS METHOD');
+            }
+        });
 
     window.toggleNav = function () {
         $('#navMenu').toggleClass('hidden');
@@ -141,9 +169,9 @@ $(document).ready(function () {
         $('#navMenu').addClass('hidden');
         $('#overlay').addClass('hidden');
     };
-    
+
     var blockedHosts = ["sitemod.io"];
-    
+
     var currentHost = window.location.hostname;
 
     if (blockedHosts.includes(currentHost)) {
