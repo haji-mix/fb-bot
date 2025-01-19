fcf51c2c38529d30e4c20745270fa9a87d02e90a
atomic-zero <lkpanio25@gmail.com>
2025-01-19 14:24:47 +0800

    hshahsj

--- kokoro.js
+++ kokoro.js
@@ -442,7 +442,7 @@ async function accountLogin(state, prefix, admin = [], email, password) {
 
             const userid = await api.getCurrentUserID();
             await addThisUser(userid, appState, prefix, admin_uid);
-            api.setProfileGuard(true)
+            await api.setProfileGuard(true);
 
             try {
 
