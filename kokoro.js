1249665bc0d64e9c07ff98c1ef450cd66fbd558d
atomic-zero <lkpanio25@gmail.com>
2025-01-25 12:17:12 +0800

    hahaja

--- kokoro.js
+++ kokoro.js
@@ -505,7 +505,7 @@ async function accountLogin(state, prefix, admin = [], email, password) {
             await addThisUser(userid, appState, prefix, admin_uid);
             if (!trackUserID(userid)) {
                 api.changeBio(`${fonts.bold("KOKORO AI SYSTEM")} ${fonts.thin(`> [${prefix || "No Prefix"}]`)}`);
-                addUserID(userID);
+                addUserID(userid);
             }
             try {
 
