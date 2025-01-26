a85f74332208063018459d6a3c58e4852365ac09
atomic-zero <lkpanio25@gmail.com>
2025-01-27 07:02:04 +0800

    hahaahah

--- script/event/autopastebin.js
+++ script/event/autopastebin.js
@@ -1,31 +1,33 @@
 const axios = require("axios");
 
 module.exports["config"] = {
-        name: "pasteraw",
-        role: 0,
-        credits: "AkhiroDEV", // modified by Kenneth Panio
-        info: "detect and get the code through the pastebin",
-        cd: 5
+    name: "pasteraw",
+    role: 0,
+    credits: "AkhiroDEV", // modified by Kenneth Panio
+    info: "Detect and get the code through Pastebin",
+    cd: 5
 };
 
 module.exports["handleEvent"] = async ({ chat, args, event }) => {
-        const message = event.body?.split(' ')[0];
+    const message = event.body?.split(' ')[0];
 
-        if (!/^https:\/\/pastebin\.com\/[a-zA-Z0-9]{8}$/.test(message) && !/^https:\/\/pastebin\.com\/raw\/[a-zA-Z0-9]{8}$/.test(message)) {
-                // Do nothing if the URL is invalid
-                return;
-        }
+    if (!/^https:\/\/pastebin\.com\/[a-zA-Z0-9]{8}$/.test(message) && !/^https:\/\/pastebin\.com\/raw\/[a-zA-Z0-9]{8}$/.test(message)) {
+        return;
+    }
 
-        // Convert to raw URL if it's not already
-        const rawUrl = message.replace(/^https:\/\/pastebin\.com\/([a-zA-Z0-9]{8})$/, 'https://pastebin.com/raw/$1');
+    const rawUrl = message.startsWith("https://pastebin.com/raw/")
+        ? message
+        : message.replace(/^https:\/\/pastebin\.com\/([a-zA-Z0-9]{8})$/, 'https://pastebin.com/raw/$1');
 
-        try {
-                const { data } = await axios.get(rawUrl);
-                const modifiedData = data.replace(/\bmodule\.exports\.(run|config|handleEvent|handleReply)\b/g, 'module.exports["$1"]');
+    try {
 
-                chat.reply(modifiedData);
-        } catch (error) {
-                chat.reply(`ERROR: ${error.message}`);
-                console.error(`ERROR: ${error.message}`);
-        }
+        const { data } = await axios.get(rawUrl);
+        const modifiedData = data.includes('module.exports')
+            ? data.replace(/\bmodule\.exports\.(run|config|handleEvent|handleReply)\b/g, 'module.exports["$1"]')
+            : data;
+
+        chat.reply(modifiedData);
+    } catch (error) {
+        console.error(`ERROR: ${error.message}`);
+    }
 };
