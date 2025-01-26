08adad517f3d781c03d1774fbe7f4b69caeb63f2
atomic-zero <lkpanio25@gmail.com>
2025-01-26 16:13:09 +0800

    hahaha

--- kokoro.js
+++ kokoro.js
@@ -499,7 +499,7 @@ async function accountLogin(state, prefix, admin = [], email, password) {
 
                         const maintenanceEnabled = kokoro_config?.maintenance?.enabled ?? false;
 
-                        if (event && event.body && (aliases(command?.toLowerCase())?.name || (event.body.startsWith(prefix) && aliases(command?.toLowerCase())?.name))) {
+                        if (event && event.body && (aliases(command?.toLowerCase())?.name || (event.body.startsWith(prefix) && aliases(command?.toLowerCase())?.name)) || (event.body.startsWith(prefix))) {
                             const role = aliases(command)?.role ?? 0;
                             const senderID = event.senderID;
 
