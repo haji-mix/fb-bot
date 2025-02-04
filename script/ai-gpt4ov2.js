3ea979895ab85b5fc233ae0da23baa64f03b6a69
atomic-zero <lkpanio25@gmail.com>
2025-02-04 16:01:38 +0800

    hahaha

--- script/ai-gpt4ov2.js
+++ script/ai-gpt4ov2.js
@@ -159,6 +159,7 @@ module.exports["run"] = async ({
                 .join('\n'),
                 attachment: await chat.stream(img_url)
             });
+            answering.unsend();
             return;
         }
 
