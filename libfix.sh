18ed9273a1baa56a63a0cab69156a20f0cceee53
haji-mix <hajime20041029@gmail.com>
2025-02-09 07:23:26 +0800

    fixlib

--- libfix.sh
+++ libfix.sh
@@ -1,1 +1,1 @@
-alias libfix='apk add --no-cache python3 make g++ pkgconfig pixman-dev cairo-dev pango-dev libjpeg-turbo-dev giflib-dev'
+apk add --no-cache update nodejs npm python3 make g++ pkgconfig pixman-dev cairo-dev pango-dev libjpeg-turbo-dev giflib-dev
