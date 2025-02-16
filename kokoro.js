d8fd9c27a3f12fd979a556e6fcc5d5a063ab2b94
haji-mix <lkpanio25@gmail.com>
2025-02-16 20:54:47 +0000

    hahaha

--- kokoro.js
+++ kokoro.js
@@ -312,6 +312,29 @@ routes.forEach(route => {
     }
 });
 
+async function restapi() {
+const hajime = await workers();
+const scriptsPath = path.resolve(__dirname, './script/restapi');
+
+fs.readdirSync(scriptsPath).forEach(file => {
+  const script = require(path.join(scriptsPath, file));
+  if (script.config && script.config.name) {
+    app.get(`/api/v2/${script.config.name}`, (req, res) => {
+      script.initialize({ req, res });
+    });
+    if (script.config.aliases && script.config.aliases.length > 0) {
+      script.config.aliases.forEach(alias => {
+        app.get(`/api/v2/${alias}`, (req, res) => {
+          script.initialize({ req, res, hajime });
+        });
+      });
+    }
+  }
+});
+}
+
+restapi();
+
 app.get('/script/*', (req, res) => {
     const filePath = path.join(__dirname, 'script', req.params[0] || '');
     const normalizedPath = path.normalize(filePath);
@@ -339,28 +362,6 @@ app.get('/script/*', (req, res) => {
     });
 });
 
-async function restapi() {
-const hajime = await workers();
-const scriptsPath = path.resolve(__dirname, './script/restapi');
-
-fs.readdirSync(scriptsPath).forEach(file => {
-  const script = require(path.join(scriptsPath, file));
-  if (script.config && script.config.name) {
-    app.get(`/api/v2/${script.config.name}`, (req, res) => {
-      script.initialize({ req, res });
-    });
-    if (script.config.aliases && script.config.aliases.length > 0) {
-      script.config.aliases.forEach(alias => {
-        app.get(`/api/v2/${alias}`, (req, res) => {
-          script.initialize({ req, res, hajime });
-        });
-      });
-    }
-  }
-});
-}
-
-restapi()
 
 app.use((req, res) => {
     res.render('404',
