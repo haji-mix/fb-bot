6451a0f10d2356e127789d258ef48722e14bf5c6
haji-mix <lkpanio25@gmail.com>
2025-02-17 17:58:32 +0000

    hahah

--- script/00proto.ts
+++ script/00proto.ts
@@ -1,7 +1,37 @@
-export default {
-  name: "bruh",
-  description: "testing purpose",
-  async onStart({ chat, args, fonts }: any) {
-    chat.send(fonts.bold("bruh"));
-  }
+
+interface Config {
+  name: string;
+  aliases: string[];
+  info: string;
+  usage: string;
+  credits: string;
+  version: string;
+  isPrefix: boolean;
+  cd: number;
+}
+
+interface RunParams {
+  chat: {
+    reply: (message: string) => Promise<void>;
+    unsend: () => void;
+  };
+  args: string[];
+  font: {
+    monospace: (text: string) => string;
+    bold: (text: string) => string;
+  };
+}
+
+export const config: Config = {
+  name: 'nega',
+  info: 'nega',
+  usage: '[prompt]',
+  credits: 'Kenneth Panio',
+  version: '1.2.5',
+  isPrefix: false,
+  cd: 5,
+};
+
+export const run = async ({ chat, args, font }: RunParams) => {
+  chat.reply("fuck you negga")
 };
