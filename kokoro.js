7d48689e2825f5f232acfaea26f44c23eb316e73
haji-mix <lkpanio25@gmail.com>
2025-02-27 00:01:03 +0000

    haha

--- kokoro.js
+++ kokoro.js
@@ -1125,20 +1125,19 @@ setInterval(executeTask, 60000);
     const ERROR = error.message || error.error || error.Error;
 
     if (ERROR_PATTERNS.errorRetrieving.test(ERROR)) {
-        logger.yellow(`Detected login issue for user ${userId}. Deleting account.`);
-        Utils.account.delete(userId);
+        logger.yellow(`Detected login issue for user ${userId}.`);
+         Utils.account.delete(userId);
         deleteThisUser(userId);
     } else if (ERROR_PATTERNS.unsupportedBrowser.test(ERROR)) {
-        logger.yellow(`Detected login browser issue for user ${userId}.`);
-      //  Utils.account.delete(userId);
-      //  deleteThisUser(userId);
+        logger.yellow(`Detected login browser issue for user ${userId}. Deleting account.`);
+        Utils.account.delete(userId);
+        deleteThisUser(userId);
     } else {
         console.error(`Can't log in user ${userId}: checkpoint status, please check your account!`, error.stack);
     }
 }
                 }
 
-                // Handle environment-based logins (each as a separate user)
                 if (process.env.APPSTATE) {
                     try {
                         const envState = JSON.parse(process.env.APPSTATE);
