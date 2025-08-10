5f8f4c5f9dce959653a6e70d6c2910932d073933
haji-mix <hajime20041029@gmail.com>
2025-08-11 07:40:35 +0800

    update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const { login } = require("./fbvibex/module/index");
+const login = require("fbvibex");
 const express = require("express");
 const nunjucks = require("nunjucks");
 require("dotenv").config();
