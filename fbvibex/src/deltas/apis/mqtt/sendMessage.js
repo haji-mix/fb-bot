bbfb193acdbef72f016067c6a9231cd2e4fcea1a
haji-mix <hajime20041029@gmail.com>
2025-08-11 07:12:31 +0800

    update

--- fbvibex/src/deltas/apis/mqtt/sendMessage.js
+++ fbvibex/src/deltas/apis/mqtt/sendMessage.js
@@ -1,857 +1,180 @@
 "use strict";
 
-
 const utils = require('../../../utils');
-const log = utils;
 
-var bluebird = require("bluebird");
+//Modified by Neth
+//iloveyou wiegine
 
-
-var allowedProperties = {
-
-  attachment: true,
-
-  url: true,
-
-  sticker: true,
-
-  emoji: true,
-
-  emojiSize: true,
-
-  body: true,
-
-  mentions: true,
-
-  location: true,
-
-};
-
-
-module.exports = function (defaultFuncs, api, ctx) {
-
+module.exports = (defaultFuncs, api, ctx) => {
+  /**
+   * Uploads an attachment to Facebook's servers.
+   * @param {Array<Stream>} attachments An array of readable streams.
+   * @param {Function} callback The callback function.
+   */
   function uploadAttachment(attachments, callback) {
-
+    callback = callback || function () {};
     var uploads = [];
-
-
-    // create an array of promises
-
     for (var i = 0; i < attachments.length; i++) {
-
       if (!utils.isReadableStream(attachments[i])) {
-
-        throw {
-
-          error:
-
-            "Attachment should be a readable stream and not " +
-
-            utils.getType(attachments[i]) +
-
-            "."
-
-        };
-
+        throw { error: "Attachment should be a readable stream and not " + utils.getType(attachments[i]) + "." };
       }
-
-
       var form = {
-
         upload_1024: attachments[i],
-
-        voice_clip: "true"
-
+        voice_clip: "true",
       };
-
-
       uploads.push(
-
         defaultFuncs
-
-          .postFormData(
-
-            "https://upload.facebook.com/ajax/mercury/upload.php",
-
-            ctx.jar,
-
-            form,
-
-            {}
-
-          )
-
+          .postFormData("https://upload.facebook.com/ajax/mercury/upload.php", ctx.jar, form, {})
           .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
-
-          .then(function (resData) {
-
-            if (resData.error) {
-
-              throw resData;
-
-            }
-
-
-            // We have to return the data unformatted unless we want to change it
-
-            // back in sendMessage.
-
+          .then(resData => {
+            if (resData.error) throw resData;
             return resData.payload.metadata[0];
-
-          })
-
+          }),
       );
-
     }
-
-
-    // resolve all promises
-
-    bluebird
-
-      .all(uploads)
-
-      .then(function (resData) {
-
-        callback(null, resData);
-
-      })
-
-      .catch(function (err) {
-
-        log.error("uploadAttachment", err);
-
+    Promise.all(uploads)
+      .then(resData => callback(null, resData))
+      .catch(err => {
+        utils.error("uploadAttachment", err);
         return callback(err);
-
       });
-
   }
 
-
-  function getUrl(url, callback) {
-
-    var form = {
-
-      image_height: 960,
-
-      image_width: 960,
-
-      uri: url
-
+  function getSendPayload(threadID, msg, otid) {
+    const isString = typeof msg === 'string';
+    const body = isString ? msg : msg.body || "";
+    let payload = {
+      thread_id: threadID.toString(),
+      otid: otid.toString(),
+      source: 0,
+      send_type: 1,
+      sync_group: 1,
+      text: body,
+      initiating_source: 1,
+      skip_url_preview_gen: 0,
     };
-
-
-    defaultFuncs
-
-      .post(
-
-        "https://www.facebook.com/message_share_attachment/fromURI/",
-
-        ctx.jar,
-
-        form
-
-      )
-
-      .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
-
-      .then(function (resData) {
-
-        if (resData.error) {
-
-          return callback(resData);
-
-        }
-
-
-        if (!resData.payload) {
-
-          return callback({ error: "Invalid url" });
-
-        }
-
-
-        callback(null, resData.payload.share_data.share_params);
-
-      })
-
-      .catch(function (err) {
-
-        log.error("getUrl", err);
-
-        return callback(err);
-
-      });
-
-  }
-
-
-  function sendContent(form, threadID, isSingleUser, messageAndOTID, callback) {
-
-    // There are three cases here:
-
-    // 1. threadID is of type array, where we're starting a new group chat with users
-
-    //    specified in the array.
-
-    // 2. User is sending a message to a specific user.
-
-    // 3. No additional form params and the message goes to an existing group chat.
-
-    if (utils.getType(threadID) === "Array") {
-
-      for (var i = 0; i < threadID.length; i++) {
-
-        form["specific_to_list[" + i + "]"] = "fbid:" + threadID[i];
-
+    if (typeof msg === 'object') {
+      if (msg.sticker) {
+        payload.send_type = 2;
+        payload.sticker_id = msg.sticker;
+        payload.text = null;
       }
-
-      form["specific_to_list[" + threadID.length + "]"] = "fbid:" + ctx.userID;
-
-      form["client_thread_id"] = "root:" + messageAndOTID;
-
-      log.info("sendMessage", "Sending message to multiple users: " + threadID);
-
-    } else {
-
-      // This means that threadID is the id of a user, and the chat
-
-      // is a single person chat
-
-      if (isSingleUser) {
-
-        form["specific_to_list[0]"] = "fbid:" + threadID;
-
-        form["specific_to_list[1]"] = "fbid:" + ctx.userID;
-
-        form["other_user_fbid"] = threadID;
-
-      } else {
-
-        form["thread_fbid"] = threadID;
-
+      if (msg.attachment) {
+        payload.send_type = 3;
+        payload.attachment_fbids = Array.isArray(msg.attachment) ? msg.attachment : [msg.attachment];
       }
-
     }
-
-
-    if (ctx.globalOptions.pageID) {
-
-      form["author"] = "fbid:" + ctx.globalOptions.pageID;
-
-      form["specific_to_list[1]"] = "fbid:" + ctx.globalOptions.pageID;
-
-      form["creator_info[creatorID]"] = ctx.userID;
-
-      form["creator_info[creatorType]"] = "direct_admin";
-
-      form["creator_info[labelType]"] = "sent_message";
-
-      form["creator_info[pageID]"] = ctx.globalOptions.pageID;
-
-      form["request_user_id"] = ctx.globalOptions.pageID;
-
-      form["creator_info[profileURI]"] =
-
-        "https://www.facebook.com/profile.php?id=" + ctx.userID;
-
-    }
-
-
-    defaultFuncs
-
-      .post("https://www.facebook.com/messaging/send/", ctx.jar, form)
-
-      .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
-
-      .then(function (resData) {
-
-        if (!resData) {
-
-          return callback({ error: "Send message failed." });
-
-        }
-
-
-        if (resData.error) {
-
-          if (resData.error === 1545003) {
-
-              log.warn(
-
-              "sendMessage",
-
-              "Got error 1545003. This might mean that conversation is no longer available or innaccessible or perhaps blocked. on " +
-
-              threadID
-
-            );
-
-          }
-
-          if (resData.error === 1545012) {
-
-            log.warn(
-
-              "sendMessage",
-
-              "Got error 1545012. This might mean that you're not part of the conversation " +
-
-              threadID
-
-            );
-
-          }
-
-          return callback(resData);
-
-        }
-
-
-        var messageInfo = resData.payload.actions.reduce(function (p, v) {
-
-          return (
-
-            {
-
-              threadID: v.thread_fbid,
-
-              messageID: v.message_id,
-
-              timestamp: v.timestamp
-
-            } || p
-
-          );
-
-        }, null);
-
-
-        return callback(null, messageInfo);
-
-      })
-
-      .catch(function (err) {
-
-        log.error("sendMessage", err);
-
-        if (utils.getType(err) == "Object" && err.error === "Not logged in.") {
-
-          ctx.loggedIn = false;
-
-        }
-
-        return callback(err);
-
-      });
-
+    return payload;
   }
 
-
-  function send(form, threadID, messageAndOTID, callback, isGroup) {
-
-    // We're doing a query to this to check if the given id is the id of
-
-    // a user or of a group chat. The form will be different depending
-
-    // on that.
-
-    if (utils.getType(threadID) === "Array") {
-
-      sendContent(form, threadID, false, messageAndOTID, callback);
-
-    } else {
-
-      if (utils.getType(isGroup) != "Boolean")
-
-        sendContent(form, threadID, threadID.length <= 15, messageAndOTID, callback);
-
-      else
-
-        sendContent(form, threadID, !isGroup, messageAndOTID, callback);
-
+  /**
+   * Sends a message to a thread via MQTT with optional sequential editing.
+   * @param {object|string} msg The message to send. Can be a string or an object.
+   * @param {string} msg.body The main text of the message.
+   * @param {*} [msg.attachment] An attachment to send.
+   * @param {*} [msg.sticker] A sticker to send.
+   * @param {*} [msg.emoji] An emoji to send.
+   * @param {string} threadID The ID of the thread.
+   * @param {string} [replyToMessage] The ID of the message to reply to.
+   */
+  return async (msg, threadID, replyToMessage, callback) => {
+    if (typeof msg !== 'string' && typeof msg !== 'object') {
+      throw new Error("Message should be of type string or object, not " + utils.getType(msg) + ".");
     }
 
-  }
-
-
-  function handleUrl(msg, form, callback, cb) {
-
-    if (msg.url) {
-
-      form["shareable_attachment[share_type]"] = "100";
-
-      getUrl(msg.url, function (err, params) {
-
-        if (err) {
-
-          return callback(err);
-
-        }
-
-
-        form["shareable_attachment[share_params]"] = params;
-
-        cb();
-
-      });
-
-    } else {
-
-      cb();
-
+    if (typeof threadID !== 'string' && typeof threadID !== 'number') {
+      throw new Error("threadID must be a string or number.");
     }
 
-  }
-
-
-  function handleLocation(msg, form, callback, cb) {
-
-    if (msg.location) {
-
-      if (msg.location.latitude == null || msg.location.longitude == null) {
-
-        return callback({ error: "location property needs both latitude and longitude" });
-
-      }
-
-
-      form["location_attachment[coordinates][latitude]"] = msg.location.latitude;
-
-      form["location_attachment[coordinates][longitude]"] = msg.location.longitude;
-
-      form["location_attachment[is_current_location]"] = !!msg.location.current;
-
-    }
-
-
-    cb();
-
-  }
-
-
-  function handleSticker(msg, form, callback, cb) {
-
-    if (msg.sticker) {
-
-      form["sticker_id"] = msg.sticker;
-
-    }
-
-    cb();
-
-  }
-
-
-  function handleEmoji(msg, form, callback, cb) {
-
-    if (msg.emojiSize != null && msg.emoji == null) {
-
-      return callback({ error: "emoji property is empty" });
-
-    }
-
-    if (msg.emoji) {
-
-      if (msg.emojiSize == null) {
-
-        msg.emojiSize = "medium";
-
-      }
-
-      if (
-
-        msg.emojiSize != "small" &&
-
-        msg.emojiSize != "medium" &&
-
-        msg.emojiSize != "large"
-
-      ) {
-
-        return callback({ error: "emojiSize property is invalid" });
-
-      }
-
-      if (form["body"] != null && form["body"] != "") {
-
-        return callback({ error: "body is not empty" });
-
-      }
-
-      form["body"] = msg.emoji;
-
-      form["tags[0]"] = "hot_emoji_size:" + msg.emojiSize;
-
-    }
-
-    cb();
-
-  }
-
-
-  function handleAttachment(msg, form, callback, cb) {
-
-    if (msg.attachment) {
-
-      form["image_ids"] = [];
-
-      form["gif_ids"] = [];
-
-      form["file_ids"] = [];
-
-      form["video_ids"] = [];
-
-      form["audio_ids"] = [];
-
-
-      if (utils.getType(msg.attachment) !== "Array") {
-
-        msg.attachment = [msg.attachment];
-
-      }
-
-
-      uploadAttachment(msg.attachment, function (err, files) {
-
-        if (err) {
-
-          return callback(err);
-
-        }
-
-
-        files.forEach(function (file) {
-
-          var key = Object.keys(file);
-
-          var type = key[0]; // image_id, file_id, etc
-
-          form["" + type + "s"].push(file[type]); // push the id
-
-        });
-
-        cb();
-
-      });
-
-    } else {
-
-      cb();
-
-    }
-
-  }
-
-
-  function handleMention(msg, form, callback, cb) {
-
-    if (msg.mentions) {
-
-      for (let i = 0; i < msg.mentions.length; i++) {
-
-        const mention = msg.mentions[i];
-
-
-        const tag = mention.tag;
-
-        if (typeof tag !== "string") {
-
-          return callback({ error: "Mention tags must be strings." });
-
-        }
-
-
-        const offset = msg.body.indexOf(tag, mention.fromIndex || 0);
-
-
-        if (offset < 0) {
-
-          log.warn(
-
-            "handleMention",
-
-            'Mention for "' + tag + '" not found in message string.'
-
-          );
-
-        }
-
-
-        if (mention.id == null) {
-
-          log.warn("handleMention", "Mention id should be non-null.");
-
-        }
-
-
-        const id = mention.id || 0;
-
-        const emptyChar = '\u200E';
-
-        form["body"] = emptyChar + msg.body;
-
-        form["profile_xmd[" + i + "][offset]"] = offset + 1;
-
-        form["profile_xmd[" + i + "][length]"] = tag.length;
-
-        form["profile_xmd[" + i + "][id]"] = id;
-
-        form["profile_xmd[" + i + "][type]"] = "p";
-
-      }
-
-    }
-
-    cb();
-
-  }
-
-
-  return function sendMessage(msg, threadID, callback, replyToMessage, isGroup) {
-
-    typeof isGroup == "undefined" ? isGroup = null : "";
-
-    if (
-
-      !callback &&
-
-      (utils.getType(threadID) === "Function" ||
-
-        utils.getType(threadID) === "AsyncFunction")
-
-    ) {
-
-      return threadID({ error: "Pass a threadID as a second argument." });
-
-    }
-
-    if (
-
-      !replyToMessage &&
-
-      utils.getType(callback) === "String"
-
-    ) {
-
-      replyToMessage = callback;
-
-      callback = undefined;
-
-    }
-
-
-    var resolveFunc = function () { };
-
-    var rejectFunc = function () { };
-
-    var returnPromise = new Promise(function (resolve, reject) {
-
-      resolveFunc = resolve;
-
-      rejectFunc = reject;
-
-    });
-
-
-    if (!callback) {
-
-      callback = function (err, data) {
-
-        if (err) return rejectFunc(err);
-
-        resolveFunc(data);
-
+    const otid = utils.generateOfflineThreadingID();
+
+    const tasks = [{
+      label: "46",
+      payload: getSendPayload(threadID, msg, otid),
+      queue_name: threadID.toString(),
+      task_id: 0,
+      failure_count: null,
+    }, {
+      label: "21",
+      payload: {
+        thread_id: threadID.toString(),
+        last_read_watermark_ts: Date.now(),
+        sync_group: 1,
+      },
+      queue_name: threadID.toString(),
+      task_id: 1,
+      failure_count: null,
+    }];
+
+    if (replyToMessage) {
+      tasks[0].payload.reply_metadata = {
+        reply_source_id: replyToMessage,
+        reply_source_type: 1,
+        reply_type: 0,
       };
-
     }
 
-
-    var msgType = utils.getType(msg);
-
-    var threadIDType = utils.getType(threadID);
-
-    var messageIDType = utils.getType(replyToMessage);
-
-
-    if (msgType !== "String" && msgType !== "Object") {
-
-      return callback({
-
-        error:
-
-          "Message should be of type string or object and not " + msgType + "."
-
-      });
-
-    }
-
-
-    // Changing this to accomodate an array of users
-
-    if (
-
-      threadIDType !== "Array" &&
-
-      threadIDType !== "Number" &&
-
-      threadIDType !== "String"
-
-    ) {
-
-      return callback({
-
-        error:
-
-          "ThreadID should be of type number, string, or array and not " +
-
-          threadIDType +
-
-          "."
-
-      });
-
-    }
-
-
-    if (replyToMessage && messageIDType !== 'String') {
-
-      return callback({
-
-        error:
-
-          "MessageID should be of type string and not " +
-
-          threadIDType +
-
-          "."
-
-      });
-
-    }
-
-
-    if (msgType === "String") {
-
-      msg = { body: msg };
-
-    }
-
-
-    var disallowedProperties = Object.keys(msg).filter(
-
-      prop => !allowedProperties[prop]
-
-    );
-
-    if (disallowedProperties.length > 0) {
-
-      return callback({
-
-        error: "Dissallowed props: `" + disallowedProperties.join(", ") + "`"
-
-      });
-
-    }
-
-
-    var messageAndOTID = utils.generateOfflineThreadingID();
-
-
-    var form = {
-
-      client: "mercury",
-
-      action_type: "ma-type:user-generated-message",
-
-      author: "fbid:" + ctx.userID,
-
-      timestamp: Date.now(),
-
-      timestamp_absolute: "Today",
-
-      timestamp_relative: utils.generateTimestampRelative(),
-
-      timestamp_time_passed: "0",
-
-      is_unread: false,
-
-      is_cleared: false,
-
-      is_forward: false,
-
-      is_filtered_content: false,
-
-      is_filtered_content_bh: false,
-
-      is_filtered_content_account: false,
-
-      is_filtered_content_quasar: false,
-
-      is_filtered_content_invalid_app: false,
-
-      is_spoof_warning: false,
-
-      source: "source:chat:web",
-
-      "source_tags[0]": "source:chat",
-
-      body: msg.body ? msg.body.toString() : "",
-
-      html_body: false,
-
-      ui_push_phase: "V3",
-
-      status: "0",
-
-      offline_threading_id: messageAndOTID,
-
-      message_id: messageAndOTID,
-
-      threading_id: utils.generateThreadingID(ctx.clientID),
-
-      "ephemeral_ttl_mode:": "0",
-
-      manual_retry_cnt: "0",
-
-      has_attachment: !!(msg.attachment || msg.url || msg.sticker),
-
-      signatureID: utils.getSignatureID(),
-
-      replied_to_message_id: replyToMessage
-
+    const form = {
+      app_id: "2220391788200892",
+      payload: {
+        tasks,
+        epoch_id: utils.generateOfflineThreadingID(),
+        version_id: "6120284488008082",
+        data_trace_id: null,
+      },
+      request_id: 1,
+      type: 3,
     };
+    const waitForDelta = async (timeout = 60*1000, interval = 500) => {
+        const startTime = Date.now();
+        while (Date.now() - startTime < timeout) {
+            const delta = api.message.get(otid);
+            if (delta){
+                return delta;
+            }
+            await new Promise(res => setTimeout(res, interval));
+       }
+       api.message.delete(otid);
+       return;
+    }
+    
+    if (msg.attachment) {
+      try {
+        const files = await new Promise((resolve, reject) => {
+          uploadAttachment(
+            Array.isArray(msg.attachment) ? msg.attachment : [msg.attachment],
+            (err, files) => {
+              if (err) return reject(err);
+              return resolve(files);
+            }
+          );
+        });
+        form.payload.tasks[0].payload.attachment_fbids = files.map(file => Object.values(file)[0]);
+      } catch (err) {
+        utils.error("Attachment upload failed:", err);
+        throw new Error(err);
+      }
+    }
 
-
-    handleLocation(msg, form, callback, () =>
-
-      handleSticker(msg, form, callback, () =>
-
-        handleAttachment(msg, form, callback, () =>
-
-          handleUrl(msg, form, callback, () =>
-
-            handleEmoji(msg, form, callback, () =>
-
-              handleMention(msg, form, callback, () =>
-
-                send(form, threadID, messageAndOTID, callback, isGroup)
-
-              )
-
-            )
-
-          )
-
-        )
-
-      )
-
-    );
-
-
-    return returnPromise;
-
+    form.payload.tasks.forEach(task => {
+      task.payload = JSON.stringify(task.payload);
+    });
+    form.payload = JSON.stringify(form.payload);
+    const data = await ctx.mqttClient.publish("/ls_req", JSON.stringify(form));
+    const deltaResponse = await waitForDelta();
+    return {
+        ...(data && { ...data }),
+        form,
+        threadID,
+        type: replyToMessage ? "message_reply" : "message",
+        ...(deltaResponse && {
+            body: deltaResponse.body,
+            attachments: deltaResponse.attachments,
+            ...(deltaResponse.messageMetadata && {
+                messageID: deltaResponse.messageMetadata.messageId,
+                offlineThreadingId: deltaResponse.messageMetadata.offlineThreadingId
+            })
+        })
+    }
   };
-
 };
